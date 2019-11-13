62fb7e0dd3348007959e8731f85df513c91e4bc4
umutykaya <yalcinkayaumut@outlook.com>
2019-11-13 09:04:14 +0300

    first commit

---
+++ build/precache-manifest.06b8c93ace0353d00253abf4d738c6f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cc1292d63246a49442714efa3fcfca9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4461a1b5b84cc9e66ad",
+    "url": "/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "58632c8616a46980d90b",
+    "url": "/static/css/main.c47cfcc9.chunk.css"
+  },
+  {
+    "revision": "c4461a1b5b84cc9e66ad",
+    "url": "/static/js/2.bacd6b28.chunk.js"
+  },
+  {
+    "revision": "58632c8616a46980d90b",
+    "url": "/static/js/main.43bc5048.chunk.js"
+  },
+  {
+    "revision": "7310bf4b914e5dff2e10",
+    "url": "/static/js/runtime-main.e5adb459.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c688e8a92d94d6c869adb6acedc3dac.js"
+  "/precache-manifest.06b8c93ace0353d00253abf4d738c6f2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
