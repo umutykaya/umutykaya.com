fdded8e3c54922d627b825bb7b3f22d0bfef26de
Umut Yalcinkaya <umutyalcinkaya@Umuts-MacBook-Pro.local>
2019-11-17 17:16:53 +0300

    folders modularized

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,7 +4,7 @@ import { Grid, Cell } from 'react-mdl';
 
 class Contact extends Component {
   render() {
-    return(
+    return (
       <div>
         <Grid className="contact-grid">
           <Cell col={6}>
@@ -12,24 +12,26 @@ class Contact extends Component {
             <img
               src="https://umutyalcinkaya.s3-eu-west-1.amazonaws.com/images/bye.png"
               alt="avatar"
-              style={{height: '250px'}}
-               />
-             <p style={{ width: '75%', margin: 'auto', paddingTop: '1em'}}>Principled software engineering, conversational assistance bots, big data cloud operations, software architecture, Postgresql database management, machine learning, end-to-end application delivery for multi environments, advanced analytics, IOT signal processing, data management, supervised learning, kaizen, strategic thinking, business logic.
-
-</p>
+              style={{ height: '250px' }}
+            />
+            <p style={{ width: '75%', margin: 'auto', paddingTop: '1em' }}>
+              I'm really curios person about technology and innovation.
+              Please feel free to chat with me about ideas.
+              I believe that innovation valuable whether it has been shared.
+            </p>
 
           </Cell>
           <Cell col={6}>
             <h2>Contact Me</h2>
-            <hr/>
+            <hr />
 
             <div className="contact-list">
-            <h4>Phone</h4>
-            <p><a href="callto://+905340345553">+90 534 034 55 53</a></p>
-            <h4>Email</h4>
-            <p><a href = "mailto:yalcinkayaumut@outlook.com">yalcinkayaumut@outlook.com</a></p>
-            <h4>Skype</h4>
-            <p><a href="skype:+905340345553?call">live:yalcinkayaumut</a></p>
+              <h4>Phone</h4>
+              <p><a href="callto://+905340345553">+90 534 034 55 53</a></p>
+              <h4>Email</h4>
+              <p><a href="mailto:yalcinkayaumut@outlook.com">yalcinkayaumut@outlook.com</a></p>
+              <h4>Skype</h4>
+              <p><a href="skype:+905340345553?call">live:yalcinkayaumut</a></p>
             </div>
           </Cell>
         </Grid>
