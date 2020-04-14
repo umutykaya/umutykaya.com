0cf2dd6eb7e141878dd85c3a98319a4726430c20
Yalcinkaya-ext, Umut (GIT-TCG3) <Umut.Yalcinkaya-ext@bshg.com>
2020-04-15 01:01:05 +0300

    current change

--- src/containers/projects.js
+++ src/containers/projects.js
@@ -131,11 +131,11 @@ class Projects extends Component {
       )
     } else if (this.state.activeTab === 2) {
       return (
-        <div><h1>This is VueJS</h1></div>
+        <div><h1>This is Java</h1></div>
       )
     } else if (this.state.activeTab === 3) {
       return (
-        <div><h1>This is MongoDB</h1></div>
+        <div><h1>This is AWS Best Architectures</h1></div>
       )
     }
 
